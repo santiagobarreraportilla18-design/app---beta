c6bcd5fa33217eb85a8a3f672e4082c029205ed5
Santi <santiago@mail.com>
2026-03-27 12:26:22 -0500

    Description2

--- src/signup.php
+++ src/signup.php
@@ -7,7 +7,14 @@
   $m_phone = $_POST['mphone'];
   $p_sswd  = $_POST['passwd'];
   $enc_pass = md5($p_sswd);
+  
+$check_phone = "SELECT mobile_phone FROM users_model WHERE mobile_phone = '$m_phone'";
+$res_phone = pg_query($local_conn, $check_phone);
 
+if (pg_num_rows($res_phone) > 0) {
+    echo "Error: El número de celular '$m_phone' ya está registrado en nuestro sistema."; 
+    exit();
+}
   //Query to insert into SQL.
 $sql = "INSERT INTO users (firstname,lastname,email,mobile_phone,password) 
        VALUES('$f_name','$l_name','$e_mail','$m_phone','$enc_pass')";
